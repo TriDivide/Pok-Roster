6093f6e8c681bc9e4d4844fbc6375e3493ed6df2
Liam Smith <liam.smith@appsinthesky.com>
2019-12-22 23:14:51 +0000

    - Worte the bear bones code for the user model

--- PokéRoster/DataModel/Models/UserModel.swift
+++ PokéRoster/DataModel/Models/UserModel.swift
@@ -5,10 +5,77 @@ class UserModel: BaseModel {
     
     public static let instance = UserModel()
     private let userRef = Firestore.firestore().collection("users")
+    private var user: User?
 
     
-    public func doLogin(email: String, password: String, completion: @escaping(User?, Error?) -> Void) {
+    public func doLogin(email: String, password: String, completion: @escaping(Error?) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) {(user, error) in
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let _ = user else {
+                completion(UserModelError.AuthenticationFailed)
+                return
+            }
+            completion(nil)
+        }
+    }
+    
+    public func doSignUp(email: String, password: String, completion: @escaping(Error?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            
+        }
+    }
+    
+    public func isLoggedIn() -> Bool {
+        return Auth.auth().currentUser != nil
+    }
+    
+    public func doLogout(completion: @escaping(Error?) -> Void) {
+        do {
+            try Auth.auth().signOut()
+            self.user = nil
+            completion(nil)
+        }
+        catch {
+            completion(error)
+        }
+    }
+    
+    public func getCurrentUser(completion: @escaping(User?, Error?) -> Void) {
+        guard let userId = Auth.auth().currentUser?.uid else {
+            completion(nil, UserModelError.NotLoggedIn)
+            return
+        }
         
+        userRef.document(userId).getDocument {(document, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            guard let document = document, let data = document.data() else {
+                completion(nil, UserModelError.UserNotFound)
+                return
+            }
+            
+            guard let email = data["email"] as? String, let username = data["username"] as? String else {
+                completion(nil, UserModelError.InvalidData)
+                return
+            }
+            
+            let user = User(email: email, username: username)
+            self.user = user
+            completion(user, nil)
+        }
         
     }
 }
--- PokéRoster/Error.swift
+++ PokéRoster/Error.swift
@@ -3,7 +3,9 @@ import Foundation
 
 enum UserModelError: Error {
     case UserNotFound
-    
+    case AuthenticationFailed
+    case NotLoggedIn
+    case InvalidData
 }
 
 extension UserModelError: LocalizedError{
@@ -11,6 +13,12 @@ extension UserModelError: LocalizedError{
         switch self {
         case .UserNotFound:
             return NSLocalizedString("User not found", comment: "UserNotFound")
+        case .AuthenticationFailed:
+            return NSLocalizedString("Authentication failed, please contact support", comment: "AuthFailed")
+        case .NotLoggedIn:
+            return NSLocalizedString("You are not currently logged in. Please log in", comment: "NotLoggedIn")
+        case .InvalidData:
+            return NSLocalizedString("User data received is invalid", comment: "InvalidData")
         }
     }
 }
