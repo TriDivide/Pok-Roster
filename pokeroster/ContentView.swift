1be2ade6e968389c348e18e151f3562e1fb6db96
Liam Smith <liam.smith@appsinthesky.com>
2020-01-14 21:56:23 +0000

    - started work on a basic UI to test the models

--- pokeroster/ContentView.swift
+++ pokeroster/ContentView.swift
@@ -9,10 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            TextField("Email", text: $email)
+            SecureField("Password", text: $password)
+            
+            Text("hello \(email)")
+            Text("Your password is: \(password)")
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Login")
+            }
+        }
+
+    }
+    
+// MARK: Logic
+    private func doLogin() {
+        guard email.count > 0 else {
+            print("name not entered")
+            return
+        }
+        
+        guard password.count >= 6 else {
+            print("Enter password")
+            return
+        }
+// doLogin(email: String, password: String, completion: @escaping(Error?) -> Void)
+        UserModel.instance.doLogin(email: email, password: password) {(error) in
+            
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
