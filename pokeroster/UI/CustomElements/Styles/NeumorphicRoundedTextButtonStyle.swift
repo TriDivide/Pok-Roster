1ae02bd170f1fdafa4c72be05f05bd4944531240
Liam Smith <liam.smith@appsinthesky.com>
2020-03-15 16:34:29 +0000

    - created the basic style for the button

---
+++ pokeroster/UI/CustomElements/Styles/NeumorphicRoundedTextButtonStyle.swift
@@ -0,0 +1,16 @@
+//
+//  NeumorphicRoundedTextButtonStyle.swift
+//  pokeroster
+//
+//  Created by Liam Smith on 15/03/2020.
+//  Copyright © 2020 Liam Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct NeumorphicRoundedTextButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+    }
+}
